a1b5d16028e4197caae994a05d5b97466a7b3f08
donsantos <donantoniosantos@gmail.com>
2022-05-10 22:30:31 -0500

    remove answers

--- gcs.tf
+++
@@ -1,7 +0,0 @@
-resource "google_storage_bucket" "main" {
-  name          = var.bucket_name
-  location      = var.bucket_location
-  force_destroy = true
-
-  uniform_bucket_level_access = true
-}
